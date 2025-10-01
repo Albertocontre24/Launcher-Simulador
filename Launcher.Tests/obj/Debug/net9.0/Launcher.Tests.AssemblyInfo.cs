fc83b7631f6244e83e8bdc4fad4fc75aec6fe6f2
luismartinezparra2005 <luismartinezparra2005@gmail.com>
2025-10-01 10:35:04 +0200

    Nueva actu

--- Launcher.Tests/obj/Debug/net9.0/Launcher.Tests.AssemblyInfo.cs
+++ Launcher.Tests/obj/Debug/net9.0/Launcher.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0451878c55aa2060606dc7601655ee619fe079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fbf77ea1b21c658581ec99f932497629fbf917")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
