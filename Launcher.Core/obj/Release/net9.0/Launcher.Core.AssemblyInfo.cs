1d7d30fd1fa3791131f02522a47a2e157d8854e1
luismartinezparra2005 <luismartinezparra2005@gmail.com>
2025-10-21 13:37:01 +0200

    Url del manifest del repositorio

--- Launcher.Core/obj/Release/net9.0/Launcher.Core.AssemblyInfo.cs
+++ Launcher.Core/obj/Release/net9.0/Launcher.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8425282e58adb247b20739846c2d38c390d7b257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a69d3c8f3b8d597eb4e9a56af7682f457f0808")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
