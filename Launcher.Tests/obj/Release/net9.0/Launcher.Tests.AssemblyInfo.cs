5d28ad4904894a4614b8f6d927b752289f762920
luismartinezparra2005 <luismartinezparra2005@gmail.com>
2025-10-27 11:31:21 +0100

    Prueba 1.4

--- Launcher.Tests/obj/Release/net9.0/Launcher.Tests.AssemblyInfo.cs
+++ Launcher.Tests/obj/Release/net9.0/Launcher.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a69d3c8f3b8d597eb4e9a56af7682f457f0808")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1ca9a46382de05cdf875b5ff4516ebdca39df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
